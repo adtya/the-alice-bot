f547a05efba516b5e0ff1aa986b346add097edc6
Adithya Nair <adtya.nair@gmail.com>
2017-12-24 19:09:56 +0530

    add databse functionality

--- index.py
+++ index.py
@@ -1,20 +1,43 @@
 import telebot
-import functions
+import sql_functions
 
 
-if __name__ == "__main__":
-    db_name = "alice.db"
-    sql = """create table Responses(
-            ID integer,
-            Name text,
-            desc text,
-            primary key(ID))"""
-    functions.create_table(db_name, 'Responses', sql)
+db_name = "alice.db"
+sql = """create table Users(
+        ID integer,
+        Name text,
+        primary key(ID))"""
+sql_functions.create_table(db_name, 'Users', sql)
 
-    bot = telebot.TeleBot('501737753:AAH_xjdeSe1pUg5cEBazOAFRTaYuqZUzbms') # Bot Token, obtained from @botfather
 
-    @bot.message_handler(commands = ['start']) # Reply to /start command
-    def welcome(message):
-        bot.send_message(message.chat.id, "Hello, How can I help?")
+bot = telebot.TeleBot('501737753:AAH_xjdeSe1pUg5cEBazOAFRTaYuqZUzbms') # Bot Token, obtained from @botfather
 
-    bot.polling(none_stop=True)
+
+@bot.message_handler(commands = ['start','hello']) # Reply to /start or /hello command
+def welcome(message):
+    chat_type = message.chat.type
+    chat_id = message.chat.id
+    f_name = message.from_user.first_name
+    l_name = message.from_user.last_name
+    name = f_name+" "+l_name
+
+    if chat_type == "private":
+        print(f_name,"with id",chat_id,"is a user.")
+        user_exist = sql_functions.check_user(db_name, 'Users', chat_id)
+        if user_exist == True:
+            print("User already exists in db.")
+        else:
+            print("Adding",f_name,"to db.")
+            sql_functions.add_user(db_name, 'Users', chat_id, name)
+
+    elif (chat_type == "group") | (chat_type == "supergroup"):
+        print(chat_id,"is a group\n")
+        bot.send_message(chat_id, 'This is a group, and I hate crowd. PM me for a better bot experience.')
+
+    # keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # keyboard.row('\U0001F4D7 Documents', '\U0001F4D7 Reminders')
+    # keyboard.row('\U0001F4D7 Today\'s Schedule', '\U0001F4D7 Attendance')
+    # bot.send_message(chat_id, 'Hello, What can I help you with?', reply_markup=keyboard)
+    # print("welcome message send to",f_name)
+
+bot.polling(none_stop=True)
---
+++ sql_functions.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def create_table(db_name, table_name, sql):
+    with sqlite3.connect(db_name) as db:
+        cursor = db.cursor()
+        cursor.execute("select name from sqlite_master where name=?", (table_name,))
+        result = cursor.fetchall()
+        if len(result) == 0:
+            cursor.execute(sql)
+            print("Table "+ table_name +" created\n")
+            db.commit();
+
+def check_user(db_name, table_name, user_id):
+    with sqlite3.connect(db_name) as db:
+        cursor = db.cursor()
+        cursor.execute("select Name from Users where ID=?", (user_id,))
+        result = cursor.fetchall()
+        if len(result) == 1:
+            return True
+        elif len(result) == 0:
+            return False
+
+def add_user(db_name, table_name, user_id, user_name):
+    with sqlite3.connect(db_name) as db:
+        cursor = db.cursor()
+        sql_exec = "insert into "+table_name+"(ID,Name)"+"\nvalues("+str(user_id)+",\'"+user_name+"\')"
+        cursor.execute(sql_exec)
+        print(user_name,"added\n")
