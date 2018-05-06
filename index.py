a6318a8d18542bb237450f0a2deffab41217bda8
Adithya Nair <adtya.nair@pm.me>
2018-05-06 21:24:19 +0530

    fix intendation

--- index.py
+++ index.py
@@ -140,4 +140,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
